4f6d4a66549a274c648f7fa965445cc8faf0e929
Andrey Znak <a.y.znak@gmail.com>
2020-10-12 19:11:35 +0300

    feat: add disicion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    str = str.split('');
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (str[i] == bracketsConfig[j][0] && str[i + 1] == bracketsConfig[j][1]) {
+                str.splice(i, 2);
+                i = -1;
+                break;
+            }
+        }
+    }
+    return str.length == 0 ? true : false;
 }
